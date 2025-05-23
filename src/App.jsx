768fe31823b4201183914e77048b70ac333d5a19
Guilherme Santos <106110470+guileals@users.noreply.github.com>
2025-05-23 17:23:16 +0000

    Aula 23/05/25 - Props, Children e renderização usando map

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,24 @@ import "./styles/global.css";
 import { MyText } from "./components/MyText";
 
 export default function App() {
+  const texts = [
+    { title: "Meu título", text: "Meu texto" },
+    { title: "Meu título", text: "Meu texto" },
+    { title: "Meu título", text: "Meu texto" },
+    { title: "Meu título", text: "Meu texto" },
+    { title: "Meu título", text: "Meu texto" },
+  ];
+
   return (
     // React Fragment
-    <>  
-      <MyText />
-      <MyText />
-      <MyText />
+    <>
+      {texts.map((text, index) => {
+        return (
+          <MyText key={index} title={`${text.title} ${index + 1}`}>
+            {text.text}
+          </MyText>
+        );
+      })}
     </>
   );
 }
--- src/components/MyText.jsx
+++ src/components/MyText.jsx
@@ -1,16 +1,11 @@
 import styles from "./MyText.module.css";
 
-export function MyText() {
+export function MyText({ title, children }) {
   return (
     <div className={styles.container}>
       <div className={styles.div}>
-        <h1 className={styles.title}>Meu primeiro React App</h1>
-        <p className={styles.text}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod ipsum,
-          aliquid alias nemo consectetur dignissimos delectus quos nisi deleniti
-          obcaecati vitae dolorem fuga, neque placeat laborum deserunt maiores
-          mollitia? Excepturi?
-        </p>
+        <h1 className={styles.title}>{title}</h1>
+        <p className={styles.text}>{children}</p>
       </div>
     </div>
   );
