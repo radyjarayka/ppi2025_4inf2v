f34aa1f884d3b3a2445ac0be59a30211dd58c53b
Guilherme Santos <106110470+guileals@users.noreply.github.com>
2025-05-29 17:17:46 +0000

    Aula 29/05/25 - CSS Flex

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,13 @@
 import "./styles/theme.css";
 import "./styles/global.css";
-import { MyText } from "./components/MyText";
+import { MyTextList } from "./components/MyTextList";
 
 export default function App() {
-  const texts = [
-    { title: "Meu título", text: "Meu texto" },
-    { title: "Meu título", text: "Meu texto" },
-    { title: "Meu título", text: "Meu texto" },
-    { title: "Meu título", text: "Meu texto" },
-    { title: "Meu título", text: "Meu texto" },
-  ];
 
   return (
     // React Fragment
     <>
-      {texts.map((text, index) => {
-        return (
-          <MyText key={index} title={`${text.title} ${index + 1}`}>
-            {text.text}
-          </MyText>
-        );
-      })}
+      <MyTextList />
     </>
   );
 }
---
+++ src/components/MyTextList.jsx
@@ -0,0 +1,25 @@
+import styles from "./MyTextList.module.css";
+import { MyText } from "./MyText";
+
+export function MyTextList() {
+  const texts = [
+    { title: "Meu título", text: "Meu texto" },
+    { title: "Meu título", text: "Meu texto" },
+    { title: "Meu título", text: "Meu texto" },
+    { title: "Meu título", text: "Meu texto" },
+    { title: "Meu título", text: "Meu texto" },
+    { title: "Meu título", text: "Meu texto" },
+  ];
+
+  return (
+    <div className={styles.container}>
+      {texts.map((text, index) => {
+        return (
+          <MyText key={index} title={`${text.title} ${index + 1}`}>
+            {text.text}
+          </MyText>
+        );
+      })}
+    </div>
+  );
+}
